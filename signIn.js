415d16e33d7f95ec33eb1c1138226f650ca7381e
abdallah-alhasan <eng.abdallahalhasan@gmail.com>
2022-04-01 02:19:33 +0300

    add

--- signIn.js
+++ signIn.js
@@ -1,4 +1,5 @@
 //validation for sign in form 
+
 let userName = document.getElementById("user")
 let signInPass = document.getElementById("signIn-pass")
 let signInSubmit = document.getElementById("signIn-submit")
@@ -6,11 +7,13 @@ let userNameSpan = document.getElementById("checkEmail")
 let signInAlert = document.getElementById("signInAlert")
 signInSubmit.onclick = function () {
     if (userName.value === localStorage.getItem("email") || userName.value === localStorage.getItem("userN")) {
-        if(signInPass.value === localStorage.getItem("password")){
-        window.location.href = "https://abdallah-alhasan.github.io/Quiz-site/sign-up.html"
-    }else{
-        signInAlert.style.display = "inline"
-    }
+        userNameSpan.style.display = "none"
+        if (signInPass.value === localStorage.getItem("password")) {
+            signInAlert.style.display = "none"
+            window.location.href = "/sign-up.html"
+        } else {
+            signInAlert.style.display = "inline"
+        }
     } else {
         userNameSpan.style.display = "inline"
     }
--- signUp.js
+++ signUp.js
@@ -3,60 +3,67 @@ let input = document.querySelectorAll(".input")
 let signUp = document.getElementById("signUp")
 let checkPass = document.getElementById("rePass");
 let submit = document.getElementById("submit");
-let fNameSpan= document.getElementById("firstName")
-let lNameSpan= document.getElementById("lastName")
-let pass= document.getElementById("pass")
-let chpass= document.getElementById("checkpass")
+let fNameSpan = document.getElementById("firstName")
+let lNameSpan = document.getElementById("lastName")
+let pass = document.getElementById("pass")
+let chpass = document.getElementById("checkpass")
 let name = document.getElementsByClassName("name");
 let mailCheck = document.querySelector(".mailCheck")
 let userN = document.getElementById("userN")
 let userNSpan = document.getElementById("userNSpan")
 let nameReg = /[a-zA-Z]/;
-let password=document.getElementById("password");
-let passwordReg= /^[A-Za-z]\w{7,14}$/;
+let password = document.getElementById("password");
+let passwordReg = /^[A-Za-z]\w{7,14}$/;
 
 
-for( i = 0 ; i < input.length ; i++){
-    input[i].onchange = function(){
-        localStorage.setItem(this.name , this.value)
-    }
+for (i = 0; i < input.length; i++) {
+  input[i].onchange = function () {
+    localStorage.setItem(this.name, this.value)
+  }
 }
 
 
-submit.onclick = function() {
-  
-  
-    if(name[0].value.match(nameReg)){
-      fNameSpan.style.display = "none"
-    }else{
-      fNameSpan.style.display = "inline"
-    }
-    if(name[1].value.match(nameReg)){
+submit.onclick = function () {
+
+
+  if (name[0].value.match(nameReg)) {
+    fNameSpan.style.display = "none"
+    if (name[1].value.match(nameReg)) {
       lNameSpan.style.display = "none"
-    }else{
+      if (userN.value.match(nameReg)) {
+        userNSpan.style.display = "none"
+        if (email.value.endsWith(".com") === false || email.value.includes("@") === false) {
+          mailCheck.style.display = "inline"
+        } else {
+          mailCheck.style.display = "none"
+          if (password.value.match(passwordReg)) {
+            pass.style.display = "none"
+            if (password.value === checkPass.value) {
+              chpass.style.display = "none"
+              window.location.href = "/sign-in.html"
+            } else {
+              chpass.style.display = "inline"
+            }
+          } else {
+            pass.style.display = "inline"
+          }
+        }
+      } else {
+        userNSpan.style.display = "inline"
+      }
+    } else {
       lNameSpan.style.display = "inline"
     }
-    if(userN.value.match(nameReg)){
-      userNSpan.style.display = "none"
-    }else{
-      userNSpan.style.display = "inline"
-    }
-    if(password.value.match(passwordReg)){
-      pass.style.display = "none"
-    }else{
-      pass.style.display = "inline"
-    }
-    if(password.value === checkPass.value){
-      chpass.style.display = "none"
-    }else{
-      chpass.style.display = "inline"
-    }
-    if (email.value.endsWith(".com") === false || email.value.includes("@") === false ){
-      mailCheck.style.display = "inline"
-    }else{
-      mailCheck.style.display = "none"
-    }
+  } else {
+    fNameSpan.style.display = "inline"
   }
+
+
+
+
+
+
+}
 
 
 
